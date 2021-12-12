65a9f6320fdcd8ded16caed0c529d1392e65612c
GBelther <belthergabriel@gmail.com>
2021-12-12 15:05:51 -0300

    feature: acrescentando novos retornos do endpoint e populando a pagina

--- src/views/Movie/index.tsx
+++ src/views/Movie/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useTheme } from "styled-components";
 import { Button } from "../../components/Button";
@@ -28,103 +28,133 @@ import {
   Title,
 } from "./styles";
 
+interface ICategory {
+  id: string;
+  name: string;
+}
+interface IMovie {
+  id: string;
+  name: string;
+  rating: number;
+  image: string;
+  description: string;
+  category: ICategory;
+  cast: string[];
+  year: number;
+  director: string;
+}
+
 const Movie = () => {
   const theme = useTheme();
   const params = useParams();
+
+  const [movieDetails, setMovieDetails] = useState<IMovie>();
 
   useEffect(() => {
     const getMovie = async () => {
       const { id }: any = params;
       const response = await apimovies.get(`movies/${id}`);
 
-      console.log(response);
+      const {
+        id: movieId,
+        name,
+        rating,
+        image,
+        description,
+        category,
+        cast,
+        year,
+        director,
+      } = response.data;
+
+      setMovieDetails({
+        id: movieId,
+        name,
+        rating,
+        image,
+        description,
+        category,
+        cast,
+        year,
+        director,
+      });
     };
 
     getMovie();
-  }, []);
+  }, [params]);
 
   return (
     <Container>
-      <MovieDetailWrapper>
-        <Details>
-          <Title>Homem-Aranha</Title>
-          <ImageMovie src="images/homem-aranha.png" />
-          <RatingWrapper>
-            <StarsRating rating={4} />
-          </RatingWrapper>
-          <InfoWrapper>
-            <Info>
-              <strong>Ano: </strong> 2021
-            </Info>
-            <Info>
-              <strong>Gênero: </strong> Aventura
-            </Info>
-            <Info>
-              <strong>Diretor: </strong> Will Smith
-            </Info>
-            <Info>
-              <strong>Elenco: </strong>
-              Jack Chan, Will Smith, Jack Chan, Will Smith, Jack Chan, Will
-              Smith, Jack Chan, Will Smith,
-            </Info>
-          </InfoWrapper>
-        </Details>
-      </MovieDetailWrapper>
-      <MovieContentWrapper>
-        <DescriptionWrapper>
-          <DescriptionHeading>Descrição do Filme</DescriptionHeading>
-          <Description>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit ab
-            minima in aperiam modi perspiciatis nemo culpa. Omnis, quis! Vitae
-            natus nostrum aliquam, dolorem laudantium laborum perferendis
-            ducimus in optio? Lorem, ipsum dolor sit amet consectetur
-            adipisicing elit. Velit ab minima in aperiam modi perspiciatis nemo
-            culpa. Omnis, quis! Vitae natus nostrum aliquam, dolorem laudantium
-            laborum perferendis ducimus in optio? Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Velit ab minima in aperiam modi
-            perspiciatis nemo culpa. Omnis, quis! Vitae natus nostrum aliquam,
-            dolorem laudantium laborum perferendis ducimus in optio? Lorem,
-            ipsum dolor sit amet consectetur adipisicing elit. Velit ab minima
-            in aperiam modi perspiciatis nemo culpa. Omnis, quis! Vitae natus
-            nostrum aliquam, dolorem laudantium laborum perferendis ducimus in
-            optio?
-          </Description>
-        </DescriptionWrapper>
-        <ReviewWrapper>
-          <AddReviewWrapper>
-            <AddReviewTitle>
-              <strong>Deixe sua avaliação</strong>
-              <StarsRating rating={3} />
-            </AddReviewTitle>
-            <ReviewField />
-            <Button backgroundColor={theme.colors.secondary}>Postar</Button>
-          </AddReviewWrapper>
-          <Reviews>
-            <Review>
-              <ReviewInformation>
-                Gabriel Belther Santos | 14/12/2021 às 12:58
-              </ReviewInformation>
-              <ReviewText>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
-                odio accusamus magnam autem eum officiis ad reiciendis, aut,
-                odit, praesentium provident soluta recusandae molestiae eos
-                doloremque. Consequuntur suscipit minus eligendi.
-              </ReviewText>
-            </Review>
-            <Review>
-              <ReviewInformation>
-                Gabriel Belther Santos | 14/12/2021 às 12:58
-              </ReviewInformation>
-              <ReviewText>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
-                odio accusamus magnam autem eum officiis ad reiciendis, aut,
-                odit, praesentium provident soluta recusandae molestiae eos
-                doloremque. Consequuntur suscipit minus eligendi.
-              </ReviewText>
-            </Review>
-          </Reviews>
-        </ReviewWrapper>
-      </MovieContentWrapper>
+      {!!movieDetails && (
+        <>
+          <MovieDetailWrapper>
+            <Details>
+              <Title>{movieDetails.name}</Title>
+              <ImageMovie src={movieDetails.image} />
+              <RatingWrapper>
+                <StarsRating rating={movieDetails.rating} />
+              </RatingWrapper>
+              <InfoWrapper>
+                <Info>
+                  <strong>Ano: </strong> {movieDetails.year}
+                </Info>
+                <Info>
+                  <strong>Gênero: </strong> {movieDetails.category.name}
+                </Info>
+                <Info>
+                  <strong>Diretor: </strong> {movieDetails.director}
+                </Info>
+                <Info>
+                  <strong>Elenco: </strong>
+                  {movieDetails.cast}
+                </Info>
+              </InfoWrapper>
+            </Details>
+          </MovieDetailWrapper>
+          <MovieContentWrapper>
+            <DescriptionWrapper>
+              <DescriptionHeading>Descrição do Filme</DescriptionHeading>
+              <Description>{movieDetails.description}</Description>
+            </DescriptionWrapper>
+            <ReviewWrapper>
+              <AddReviewWrapper>
+                <AddReviewTitle>
+                  <strong>Deixe sua avaliação</strong>
+                  <StarsRating rating={3} />
+                </AddReviewTitle>
+                <ReviewField />
+                <Button backgroundColor={theme.colors.secondary}>Postar</Button>
+              </AddReviewWrapper>
+              <Reviews>
+                <Review>
+                  <ReviewInformation>
+                    Gabriel Belther Santos | 14/12/2021 às 12:58
+                  </ReviewInformation>
+                  <ReviewText>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Sequi odio accusamus magnam autem eum officiis ad
+                    reiciendis, aut, odit, praesentium provident soluta
+                    recusandae molestiae eos doloremque. Consequuntur suscipit
+                    minus eligendi.
+                  </ReviewText>
+                </Review>
+                <Review>
+                  <ReviewInformation>
+                    Gabriel Belther Santos | 14/12/2021 às 12:58
+                  </ReviewInformation>
+                  <ReviewText>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Sequi odio accusamus magnam autem eum officiis ad
+                    reiciendis, aut, odit, praesentium provident soluta
+                    recusandae molestiae eos doloremque. Consequuntur suscipit
+                    minus eligendi.
+                  </ReviewText>
+                </Review>
+              </Reviews>
+            </ReviewWrapper>
+          </MovieContentWrapper>
+        </>
+      )}
     </Container>
   );
 };
